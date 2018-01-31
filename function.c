0fe22cda2cbbad84f3311960e11059700760ce5f
Jaspreet Bassi <jaspreetsinghbassi@hotmail.com>
2018-01-31 15:07:12 +0000

    Added function program with addtion

---
+++ function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int opt;
+	int var1;
+	int var2;
+	int i, n, sum = 0, num;
+	float avg;	
+	
+	printf("1: Addition\n");
+	printf("Enter the number correspoding to the function you want to perform:\n");
+	scanf("%d",&opt);
+	
+	switch(opt)
+	{	
+		//Addition function
+		case 1:
+			printf("Enter 2 variables:\n");
+			scanf("%d%d", &var1, &var2);
+
+			printf("%d + %d = %d\n", var1, var2, var1 + var2);
+		break;
+		
