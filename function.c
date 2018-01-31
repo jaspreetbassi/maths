87a5ef45dd432c1666fd15eff70c3f8692cd8595
Jaspreet Bassi <jaspreetsinghbassi@hotmail.com>
2018-01-31 15:10:51 +0000

    Added subtraction function in function.c

--- function.c
+++ function.c
@@ -7,7 +7,7 @@ int main()
 	int i, n, sum = 0, num;
 	float avg;	
 	
-	printf("1: Addition\n");
+	printf("1: Addition\n2: Subtraction\n");
 	printf("Enter the number correspoding to the function you want to perform:\n");
 	scanf("%d",&opt);
 	
@@ -21,6 +21,14 @@ int main()
 			printf("%d + %d = %d\n", var1, var2, var1 + var2);
 		break;
 		
+		//Subtraction function
+		case 2:
+			printf("Enter 2 variables:\n");
+			scanf("%d%d", &var1, &var2);
+
+			printf("%d - %d = %d\n", var1, var2, var1 - var2);
+		break;
+
 
 	}	
 	return 0;
